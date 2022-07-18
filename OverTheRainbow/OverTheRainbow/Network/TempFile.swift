1557e9d9bc93b0f3aed94d55f4914a291caef056
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-07-18 16:28:15 +0900

    [Chore] create directories

---
+++ OverTheRainbow/OverTheRainbow/Network/TempFile.swift
@@ -0,0 +1,8 @@
+//
+//  TempFile.swift
+//  OverTheRainbow
+//
+//  Created by Jihye Hong on 2022/07/18.
+//
+
+import Foundation
---
+++ OverTheRainbow/OverTheRainbow/Screens/letterList/LetterListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LetterListViewController.swift
+//  OverTheRainbow
+//
+//  Created by Jihye Hong on 2022/07/18.
+//
+
+import Foundation
