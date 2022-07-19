8d8c60370a7b11e281fb72ec1320487b83d757ea
EuniceNam <caellime@gmail.com>
2022-07-19 11:23:17 +0900

    feat: 네비게이션컨트롤, 메인뷰 틀 추가
    + 제스처 recognizer 추가: 서브뷰 대신 스텁뷰 연결해서 테스트
    + 네비게이션 바를 메인에서 감추고 메인뷰 나갈 때 사라지도록 설정
    기타) 천국뷰 애니메이션 미구현

---
+++ OverTheRainbow/OverTheRainbow/Screens/mainView/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  OverTheRainbow
+//
+//  Created by Hyorim Nam on 2022/07/19.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // navigation bar hide and show ref: https://stackoverflow.com/a/29953818/6183323
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+}
