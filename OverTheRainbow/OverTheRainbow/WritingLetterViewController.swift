2f8da2e83542e7bb9d220654b77373267797e580
dgfghsjd <dgfghsjd@naver.com>
2022-07-25 06:12:32 +0900

    [Feat] 사진 추가 기능(#19) - 사진 앱과 연결 - 배경색 입히기

--- OverTheRainbow/OverTheRainbow/WritingLetterViewController.swift
+++ OverTheRainbow/OverTheRainbow/WritingLetterViewController.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
 
 class WritingLetterViewController: UIViewController, UITextViewDelegate {
+    
     @IBOutlet weak var writingLetterNavBar: UINavigationItem!
     @IBOutlet weak var navBarRightItem: UIBarButtonItem!
     @IBOutlet weak var openGallery: UIImageView!
     @IBOutlet weak var letterTitle: UITextField!
     @IBOutlet weak var letterContent: UITextView!
     @IBOutlet weak var writingDate: UILabel!
+    @IBOutlet weak var selectPicture: UIButton!
     var button = UIButton(type: .system)
     let date = Date()
 
@@ -24,9 +28,18 @@ class WritingLetterViewController: UIViewController, UITextViewDelegate {
         button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
         button.setTitle("취소", for: .normal)
         button.sizeToFit()
+        button.tintColor = UIColor(named: "textColor")
+
         writingLetterNavBar.leftBarButtonItem = UIBarButtonItem(customView: button)
-        openGallery.backgroundColor=UIColor.red
         openGallery.layer.cornerRadius = 10
+
+        let largeSymbol = UIImage.SymbolConfiguration(pointSize: 86, weight: .bold, scale: .large)
+        let largeBoldButton = UIImage(systemName: "plus.square.dashed", withConfiguration: largeSymbol)
+
+        selectPicture.setImage(largeBoldButton, for: .normal)
+        selectPicture.tintColor = UIColor(named: "textColor")
+        selectPicture.addTarget(self, action: #selector(pickImage), for: .touchUpInside)
+        
 
         letterTitle.placeholder = "제목을 작성해주세요"
 
@@ -43,18 +56,58 @@ class WritingLetterViewController: UIViewController, UITextViewDelegate {
 }
 
 extension ViewController :  UITextViewDelegate {
-    
     func textViewDidBeginEditing(_ textView: UITextView) {
         if textView.textColor == UIColor.lightGray {
             textView.text = nil
             textView.textColor = UIColor.black
         }
     }
-    
+
     func textViewDidEndEditing(_ textView: UITextView) {
         if textView.text.isEmpty {
             textView.text = "일반 TextView 입니다."
             textView.textColor = UIColor.lightGray
         }
+    }
+}
+
+@available(iOS 14, *)
+extension WritingLetterViewController: PHPickerViewControllerDelegate {
+    @objc func pickImage(sender: UIButton){
+        selectPicture = sender
+            var configuration = PHPickerConfiguration()
+//    이미지 정보를 가지고 올 필요가 있을땐 photolibarary 를 사용해준다. //use when need image file info.
+//            let photoLibrary = PHPhotoLibrary.shared()
+//            var configuration = PHPickerConfiguration(photoLibrary: photoLibrary)
+
+            configuration.selectionLimit = 1 //한번에 가지고 올 이미지 갯수 제한 //limit selectable image counts
+            configuration.filter = .any(of: [.images])
+            
+            let picker = PHPickerViewController(configuration: configuration)
+            picker.delegate = self
+            self.present(picker, animated: true, completion: nil)
+    }
+    
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+          
+        var imgData: Data?
+        var img: UIImage?
+        
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        let itemProvider = results.first?.itemProvider
+        
+        if let itemProvider = itemProvider,
+           itemProvider.canLoadObject(ofClass: UIImage.self){
+            itemProvider.loadObject(ofClass: UIImage.self) {
+                (image, error) in
+                DispatchQueue.main.async {
+                    self.openGallery.image = image as? UIImage
+                }
+            }
+        }
+       
     }
 }
