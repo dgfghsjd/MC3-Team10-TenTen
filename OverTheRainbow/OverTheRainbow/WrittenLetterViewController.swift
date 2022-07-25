cc70262d7665468e50d2c4860ac326ab7f59928e
dgfghsjd <dgfghsjd@naver.com>
2022-07-25 10:25:01 +0900

    [Chore] add title

--- OverTheRainbow/OverTheRainbow/WrittenLetterViewController.swift
+++ OverTheRainbow/OverTheRainbow/WrittenLetterViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 
 class WrittenLetterViewController: UIViewController {
-
+    @IBOutlet weak var selectedLetterNavBar: UINavigationItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        selectedLetterNavBar.title="편지 상세"
         // Do any additional setup after loading the view.
     }
 
